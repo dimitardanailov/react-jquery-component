a8b64587642e3ea5ec7e6562ae7eb7ac1a8a5b26
Dimitar Danailov <dimityr.danailov@gmail.com>
2018-01-29 13:56:15 +0200

    I added a sub component

--- develop/elements/App.jsx
+++ develop/elements/App.jsx
@@ -1,30 +1,11 @@
 import React, {Component} from 'react';
-import { Subject } from 'rxjs';
+import ToggleButton from './ToggleButton.jsx';
 
 export default class App extends Component {
-
-	constructor(props) {
-		super(props);
-
-		this.state = { isToggleOn: true };
-
-		this.handleClick = new Subject();
-	}
-
-	componentDidMount() {
-		this.handleClick.subscribe((prevState) => {
-			this.setState(prevState => ({
-				isToggleOn: !prevState.isToggleOn
-			}));
-		});
-	}
-
   render() {
     return (
 			<div>
-				<button onClick={() => this.handleClick.next(this.state.isToggleOn)}>
-					{this.state.isToggleOn ? 'ON': 'OFF'}
-				</button>
+				<ToggleButton  />
 			</div>
 		);
   }
---
+++ develop/elements/ToggleButton.jsx
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import { Subject } from 'rxjs';
+
+export default class ToggleButton extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = { isToggleOn: true };
+
+		this.updateToggleProperty = new Subject();
+	}
+
+	componentDidMount() {
+		this.updateToggleProperty.subscribe((prevState) => {
+			this.setState(prevState => ({
+				isToggleOn: !prevState.isToggleOn
+			}));
+		});
+	}
+
+  render() {
+    return (
+			<div>
+				<button onClick={() => this.updateToggleProperty.next(this.state.isToggleOn)}>
+					{this.state.isToggleOn ? 'ON': 'OFF'}
+				</button>
+			</div>
+		);
+  }
+}
