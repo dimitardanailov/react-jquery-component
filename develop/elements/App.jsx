eeb35f7778198ba2adcbc8f8df269c7336fa8da1
Dimitar Danailov <dimityr.danailov@gmail.com>
2018-01-29 12:56:08 +0200

    Transform onlick handler to use rxjs

--- develop/elements/App.jsx
+++ develop/elements/App.jsx
@@ -1,25 +1,28 @@
 import React, {Component} from 'react';
+import { Subject } from 'rxjs';
 
 export default class App extends Component {
+
 	constructor(props) {
 		super(props);
 
-		this.state = { 'isToggleOn': true };
+		this.state = { isToggleOn: true };
 
-		// This binding is necessary to make `this` work in the callback
-		this.handleClick = this.handleClick.bind(this);
+		this.handleClick = new Subject();
 	}
 
-	handleClick() {
-		this.setState(prevState => ({
-			isToggleOn: !prevState.isToggleOn
-		}));
+	componentDidMount() {
+		this.handleClick.subscribe((prevState) => {
+			this.setState(prevState => ({
+				isToggleOn: !prevState.isToggleOn
+			}));
+		});
 	}
 
   render() {
     return (
 			<div>
-				<button onClick={this.handleClick}>
+				<button onClick={() => this.handleClick.next(this.state.isToggleOn)}>
 					{this.state.isToggleOn ? 'ON': 'OFF'}
 				</button>
 			</div>
